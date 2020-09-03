4eb1c62113ac212ef5a942f161cd1ba970bbc41a
Samuel Watkins <samwatkins@berkeley.edu>
2020-09-03 22:47:14 +0000

    update automated plotting for README plots

--- .travis/update_plots.py
+++ .travis/update_plots.py
@@ -4,24 +4,61 @@ import datetime
 import matplotlib.pyplot as plt
 import covid
 
-jhu_data = covid.get_data()
-ax = jhu_data[jhu_data.county=='Alameda'].plot('date', 'cases', marker='.', color='r')
-ax = jhu_data[jhu_data.county=='Alameda'].plot('date', 'deaths', marker='.', ax=ax, color='k')
-
-labels = ["Cases", "Deaths"]
-for line, label in zip(ax.lines, labels):
-    line.set_label(label)
-
-ax.legend(loc='upper left')
-ax.set_yscale('log')
-ax.set_ylim(0.8)
-ax.tick_params(which='both', direction='in', top=True, right=True)
-ax.grid(linestyle='dotted', color='k')
-ax.set_ylabel('Number of Confirmed Cases/Deaths')
-ax.set_xlabel('Date')
+fig, ax = covid.plot_bay_cases(
+    cumulative=True,
+    cases=True,
+    deaths=True,
+    groupcounties=True,
+    lastnumdays=None,
+    data_source='jhu',
+)
 ax.set_title(
-    "Alameda County COVID-19 Stats\n"
+    "Bay Area COVID-19 Stats\n"
     f"Source: JHU, Last Updated: {datetime.date.today().strftime(format='%Y-%m-%d')}"
 )
-fig = plt.gcf()
-fig.savefig(".travis/current_alameda_cases.png", dpi=200)
+fig.savefig(".travis/current_bay_area_total_cases.png", dpi=200)
+
+fig, ax = covid.plot_bay_cases(
+    cumulative=False,
+    cases=True,
+    deaths=True,
+    groupcounties=True,
+    lastnumdays=None,
+    data_source='jhu',
+)
+ax.set_title(
+    "Bay Area COVID-19 Stats\n"
+    f"Source: JHU, Last Updated: {datetime.date.today().strftime(format='%Y-%m-%d')}"
+)
+fig.savefig(".travis/current_bay_area_new_cases.png", dpi=200)
+
+
+fig, ax = covid.plot_bay_cases(
+    cumulative=True,
+    cases=True,
+    deaths=True,
+    groupcounties=False,
+    lastnumdays=None,
+    data_source='jhu',
+)
+fig.savefig(".travis/current_county_total_cases.png", dpi=200)
+
+fig, ax = covid.plot_bay_cases(
+    cumulative=False,
+    cases=True,
+    deaths=False,
+    groupcounties=False,
+    lastnumdays=None,
+    data_source='jhu',
+)
+fig.savefig(".travis/current_county_new_cases.png", dpi=200)
+
+fig, ax = covid.plot_bay_cases(
+    cumulative=False,
+    cases=True,
+    deaths=False,
+    groupcounties=False,
+    lastnumdays=31,
+    data_source='jhu',
+)
+fig.savefig(".travis/current_county_new_cases_month.png", dpi=200)
--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# covid
+# Bay Area COVID-19 Dashboard
 A repo for plotting and analyzing COVID-19 cases, with a focus on the bay area.
 
-![Alameda County](.travis/current_alameda_cases.png)
+## Current Bay Area Numbers
+
+Below, we show the total, cumulative cases (and deaths) over time for the combined 9 bay area counties.
+
+![Bay Area Cumulative](.travis/current_bay_area_total_cases.png)
+
+Below, we show the daily new cases (and deaths) over time for the combined 9 bay area counties.
+
+![Bay Area Daily](.travis/current_bay_area_new_cases.png)
+
+## County-by-County Numbers
+
+Below, we show the total, cumulative cases (and deaths) over time for each of the 9 bay area counties.
+
+![County Cumulative](.travis/current_county_total_cases.png)
+
+Below, we show the daily new cases cases over time for each of the 9 bay area counties.
+
+![County Daily](.travis/current_county_new_cases.png)
+
+Below, we show the daily new cases cases over the last month for each of the 9 bay area counties.
+
+![County Daily, Recent](.travis/current_county_new_cases_month.png)
